fd2fbfa09e20ee2af5f16488e9abc6354e0d514c
Dap9 <tandonjayant@gmail.com>
2024-08-05 00:28:37 +0300

    add tpm cloning and setup instructions

--- .install.fish
+++ .install.fish
@@ -1,7 +1,10 @@
 #!/bin/env fish
 
 function __install -d "Install tmux"
+  clone_branch "git@github.com:tmux-plugins/tpm.git" "$HOME/.tmux/plugins/tpm" "master"
   printf "Installing tmux...\n"
   sudo apt install tmux -y
+
+  print_warning "Run <prefix>+I to install all plugins"
   return $status
 end
